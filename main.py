9c778d486ae040a9e3ae94136928c3fad648ed0e
Konstantin <126085191+KST105@users.noreply.github.com>
2025-12-31 12:02:32 +0300

    функции площади

---
+++ main.py
@@ -0,0 +1,12 @@
+def square_area(side):
+    side **= 2
+    return side
+
+def rectangle_area(height, width):
+    area = height * width
+    return area
+
+def circle_area(radius):
+    P = 3.14
+    area = P * radius ** 2
+    return area 

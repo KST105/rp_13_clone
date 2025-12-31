5b63c946276625a4d2deaab0d015a00d5a885059
Konstantin <126085191+KST105@users.noreply.github.com>
2025-12-31 12:02:54 +0300

    функции периметра

---
+++ perimeter.py
@@ -0,0 +1,13 @@
+def square_perimeter(side):
+    perim = side * 4
+    return perim
+
+def rectangle_perimeter(height, width):
+    perim = height * 2 + width * 2
+    return perim
+
+def circle_area(radius):
+    P = 3.14
+    perim = 2* P * radius
+    return perim
+
